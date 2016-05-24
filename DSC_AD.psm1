d68eafae8647829a57b641b15fc94b7e88f78cdc
Jason Ryberg <jason.ryberg@gmail.com>
2016-05-24 13:54:32 -0700

    style change

--- DSC_AD.psm1
+++ DSC_AD.psm1
@@ -18,7 +18,7 @@ function New-DscConfigData
     $configData = @{
         AllNodes = @(
             @{
-                NodeName='*'
+                NodeName = "*"
             }
         )
     }
