774f1a7a6772b23cea4aaca42fa72b395dc7bd63
Jason Ryberg <jason.ryberg@gmail.com>
2016-05-24 12:46:53 -0700

    Creating DSC_AD repo

---
+++ New-DSCConfigData.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "New-DSCConfigData" {
+    It "Returns Computer objects from AD in DSC Configuration Data compatible format." {
+        (New-DSCConfigData).GetType().Name -as [string] | Should Be 'hashtable'
+    }
+    It "Contains more than just the AllNodes data hash." {
+        (New-DSCConfigData).allnodes.count -as [integer] | Should BeGreaterThan 1
+    }
+}
---
+++ New-DSCConfigData.ps1
@@ -0,0 +1,36 @@
+﻿<#
+    .SYNOPSIS Returns Computer objects from AD in proper DSC Configuration Data format.  The ObjectGUID is used as the DSC ConfigurationID.
+    .PARAM
+        ADAttribute Active Directory attribute that specifies the attribute the DSC NodeType is stored in.  Defaults to Description attribute.
+    .EXAMPLE
+        New-DscConfigData -ADAttribute "division"
+#>
+function New-DscConfigData
+{
+    param
+    (
+        # ADAttribute specifies the attribute the Node Type is stored in.  Defaults to Description.
+        [Parameter()]
+        [string] $ADAttribute = "Description"
+    )
+    
+    $domainComputers = Get-ADComputer -Filter * -Properties Name, ObjectGUID, $ADAttribute
+    $configData = @{
+        AllNodes = @(
+            @{
+                NodeName='*'
+            }
+        )
+    }
+    $domainComputers | % { 
+        $configData.AllNodes += @(
+            @{
+                NodeName = $_.Name
+                ConfigurationID = $_.ObjectGUID
+                NodeType = $_.$ADAttribute
+            }
+        )
+    }
+    
+    return $configData
+}
