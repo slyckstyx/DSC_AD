d3b13e8ffd53a0e8ec9e1bb4ad22b93be10796e3
Jason Ryberg <jason.ryberg@gmail.com>
2016-07-06 10:24:17 -0700

    updated help

--- DSC_AD.psm1
+++ DSC_AD.psm1
@@ -2,13 +2,16 @@
 #Requires -Modules ActiveDirectory
 <#
     .SYNOPSIS Returns Computer objects from AD in proper DSC Configuration Data format.
-    .PARAM
+
+    .DESCRIPTION Returns Computer objects from AD in proper DSC Configuration Data format.
+
+    .PARAMETER
         ADAttribute Active Directory attribute that specifies the attribute the DSC NodeType is stored in.  Defaults to Description attribute.
-    .PARAM
+    .PARAMETER
         Filter Allows passing in a filter query string if desired.  Defaults to "*".
-    .PARAM
+    .PARAMETER
         TargetGUID Allows selection of an AD attribute other than ObjectGUID for security purposes.  Defaults to "ObjectGUID".
-    .PARAM
+    .PARAMETER
         AllNodesProperty Allows addition of extra AllNodes property values if desired.
     .EXAMPLE
         New-DscConfigData -ADAttribute "division"
@@ -53,7 +56,7 @@ function New-DscConfigData
     }
     
     #Populate Configuration Data variable with domain computers and properties
-    $domainComputers | % { 
+    $domainComputers | ForEach-Object { 
         $configData.AllNodes += @(
             @{
                 NodeName        = $_.Name
